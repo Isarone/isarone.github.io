b63d21e31b3c05acd2cb27a14693be27ed7d0957
Samuel Arone <samu.arone@gmail.com>
2024-02-27 18:44:58 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const div = document.querySelector("#contenido");
+let navegador = navigator.userAgent;
+if (
+  navigator.userAgent.match(/Android/i) ||
+  navigator.userAgent.match(/webOS/i) ||
+  navigator.userAgent.match(/iPhone/i) ||
+  navigator.userAgent.match(/iPad/i) ||
+  navigator.userAgent.match(/iPod/i) ||
+  navigator.userAgent.match(/BlackBerry/i) ||
+  navigator.userAgent.match(/Windows Phone/i)
+) {
+  console.log("Estás usando un dispositivo móvil!!");
+  div.innerHTML="Es movil";
+} else {
+  console.log("No estás usando un móvil");
+  div.innerHTML="NO es movil";
+}
