7afcccdd248f1ceaada5cb546bcb4948cbc4f2a0
Samuel Arone <samu.arone@gmail.com>
2024-02-28 16:57:16 -0500

    update js
    reconocer el webview

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const div = document.querySelector("#contenido");
+const div = document.querySelector("#divNavegador");
 let navegador = navigator.userAgent;
 if (
   navigator.userAgent.match(/Android/i) ||
@@ -10,8 +10,37 @@ if (
   navigator.userAgent.match(/Windows Phone/i)
 ) {
   console.log("Estás usando un dispositivo móvil!!");
-  div.innerHTML="Es movil";
+  div.innerHTML = "Es movil";
 } else {
   console.log("No estás usando un móvil");
-  div.innerHTML="NO es movil";
+  div.innerHTML = "NO es movil";
+}
+
+function isWebView() {
+  const htmlEl = document.getElementsByTagName("html")[0];
+  const bodyEl = document.getElementsByTagName("body")[0];
+
+  const oldHtmlHeight = htmlEl.style.height;
+  const oldBodyHeight = bodyEl.style.height;
+
+  htmlEl.style.height = "100vh";
+  bodyEl.style.height = "100%";
+
+  const webViewMode =
+    document.documentElement.clientHeight ===
+    document.documentElement.scrollHeight;
+
+  // restore height
+  htmlEl.style.height = oldHtmlHeight;
+  bodyEl.style.height = oldBodyHeight;
+
+  return webViewMode;
+}
+const divWebView = document.querySelector("#divNWebView");
+if (isWebView()) {
+  console.log("Estás dentro de un webview");
+  divWebView.innerHTML = "Estas dentro de un webview";
+} else {
+  console.log("NO Estás dentro de un webview");
+  divWebView.innerHTML = "NO es Webview";
 }
