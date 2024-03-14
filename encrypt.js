391b7bee37cbd3e4e5d1921ac624bfaa38cf567b
Samuel Arone <samu.arone@gmail.com>
2024-03-14 17:51:54 -0500

    prueba 5

--- encrypt.js
+++ encrypt.js
@@ -7,7 +7,7 @@
 document.getElementById("btnTest").addEventListener("click", test);
 
 function test() {
-  var cadena = document.querySelector("txtInput").value;
+  var cadena = document.querySelector("#txtInput").value;
   document.querySelector("#output").innerHTML = cadena;
   console.log(cadena);
 }
