78cc816db9e36a8f5eec95b25b95c1d6e38ed08a
Samuel Arone <samu.arone@gmail.com>
2024-04-04 10:34:28 -0500

    mi-espacio-privado test1

---
+++ mi-espacio-privado.js
@@ -0,0 +1,8 @@
+function addEspacioPrivado() {
+  window.localStorage.setItem("app-embedded", "on");
+  const url =
+    "https://web-test.estarbien.com.pe/mi-zona-privada/?token=OHNCanQ3Sk9VbURhN0N1QUt4Z3FvU0pQeHJZRmFQa0dLUEJjY1FvQU8zZytmeDU1ZjJ5S2FrWk10WWMzOGFtZ3paNHFXMytDbEVlcCtycUJyNUIvVklLQXo3K3NvbDR2RWRZbmxHYWlld1VFWmZRL1NIcE1pSnNyZmpoK2ZldFcvaDRlWHI2cFk1enQ4L2l3WVhjdDVBPT0wMjZkM2I1ZTU0YTUwMDk1MjZmN2YwMjM5MzZmOTQwZmI0NWM4ZTNiM2UxMTczZDBjMThkMTI1ODY2NDgyMjBj";
+  window.location.href = url;
+}
+
+addEspacioPrivado();
