623efe8af9af9c6585896b37be7d4064aef20ad3
Samuel Arone <samu.arone@gmail.com>
2024-05-09 13:56:33 -0500

    test1.3-zona-no-cliente

--- zona-no-cliente.js
+++ zona-no-cliente.js
@@ -1,8 +1,14 @@
 window.localStorage.setItem("app-embedded", "on");
+
 const urlBase = "https://b58xfhbh-3001.brs.devtunnels.ms";
 const page = "/bienestar-en-vivo";
-const token =
-  "/?token=aFdDaWNjL0pITER6dnBVdnhvRFUreVRKR1RUM3lSYWlXQWE1VktGRktEbjl0VlZvdXpqRVo2TVhnT0pheTZTRFJBSyt1Z09UaFAvOUU1ai9kdjZQNE53VFFrNjNnOWZ2V21wWCs4Z2VGTTF6aCtXd25ZZUgrdXJSSmJuV0VOb09UUXhtM0FIVUdob1ZVQ2JOSFlrak9BPT05MTg0MzBiYzI4NDkzNDVjMGZkNmQyODkyZTIzMTZjOGFiY2RjNjg3MjE2YWYzMWE0MGQzNDlkODBmNGRjMzhid";
+const input = document.getElementById("txtInput").value;
+const token = input
+  ? `/?token=${input}`
+  : "/?token=aFdDaWNjL0pITER6dnBVdnhvRFUreVRKR1RUM3lSYWlXQWE1VktGRktEbjl0VlZvdXpqRVo2TVhnT0pheTZTRFJBSyt1Z09UaFAvOUU1ai9kdjZQNE53VFFrNjNnOWZ2V21wWCs4Z2VGTTF6aCtXd25ZZUgrdXJSSmJuV0VOb09UUXhtM0FIVUdob1ZVQ2JOSFlrak9BPT05MTg0MzBiYzI4NDkzNDVjMGZkNmQyODkyZTIzMTZjOGFiY2RjNjg3MjE2YWYzMWE0MGQzNDlkODBmNGRjMzhid";
 const url = `${urlBase}${page}${token}`;
 
-window.location.href = url;
+const btn = document.getElementById("btnTest");
+btn.addEventListener("click", function () {
+  window.location.href = url;
+});
