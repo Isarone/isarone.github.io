106642394fd964cfec431e623dfd34b1407d7789
Samuel Arone <samu.arone@gmail.com>
2024-05-09 13:33:45 -0500

    test1.1-zona-no-cliente

--- zona-no-cliente.js
+++ zona-no-cliente.js
@@ -1,4 +1,5 @@
+window.localStorage.setItem("app-embedded", "on");
 
-const url = `https://b58xfhbh-3001.brs.devtunnels.ms/`
+const url = `https://b58xfhbh-3001.brs.devtunnels.ms/`;
 
 window.location.href = url;
