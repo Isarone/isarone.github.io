102f8db4c3e07a78b93131fc1486d7fdc8fcaa52
Samuel Arone <samu.arone@gmail.com>
2024-05-16 14:12:25 -0500

    test 1.0 politica de privacidad

--- zona-no-cliente.js
+++ zona-no-cliente.js
@@ -1,6 +1,5 @@
 window.localStorage.setItem("app-embedded", "on");
 
-
 /*
 • Caso 4:  Hash
 
@@ -12,13 +11,15 @@ window.localStorage.setItem("app-embedded", "on");
     
 */
 
-const urlBase = "https://b58xfhbh-3001.brs.devtunnels.ms";
-const page = "/bienestar-en-vivo";
-const input = document.getElementById("txtInput").value;
-const token = input
-  ? `/?token=${input}`
-  : "/?token=aFdDaWNjL0pITER6dnBVdnhvRFUreVRKR1RUM3lSYWlXQWE1VktGRktEbjl0VlZvdXpqRVo2TVhnT0pheTZTRFJBSyt1Z09UaFAvOUU1ai9kdjZQNE53VFFrNjNnOWZ2V21wWCs4Z2VGTTF6aCtXd25ZZUgrdXJSSmJuV0VOb09UUXhtM0FIVUdob1ZVQ2JOSFlrak9BPT05MTg0MzBiYzI4NDkzNDVjMGZkNmQyODkyZTIzMTZjOGFiY2RjNjg3MjE2YWYzMWE0MGQzNDlkODBmNGRjMzhid";
-const url = `${urlBase}${page}${token}`;
+// const urlBase = "https://b58xfhbh-3001.brs.devtunnels.ms";
+// const page = "/bienestar-en-vivo";
+// const input = document.getElementById("txtInput").value;
+// const token = input
+//   ? `/?token=${input}`
+//   : "/?token=aFdDaWNjL0pITER6dnBVdnhvRFUreVRKR1RUM3lSYWlXQWE1VktGRktEbjl0VlZvdXpqRVo2TVhnT0pheTZTRFJBSyt1Z09UaFAvOUU1ai9kdjZQNE53VFFrNjNnOWZ2V21wWCs4Z2VGTTF6aCtXd25ZZUgrdXJSSmJuV0VOb09UUXhtM0FIVUdob1ZVQ2JOSFlrak9BPT05MTg0MzBiYzI4NDkzNDVjMGZkNmQyODkyZTIzMTZjOGFiY2RjNjg3MjE2YWYzMWE0MGQzNDlkODBmNGRjMzhid";
+// const url = `${urlBase}${page}${token}`;
+
+const url = "http://localhost:3001/registro-app/";
 
 const btn = document.getElementById("btnTest");
 btn.addEventListener("click", function () {
