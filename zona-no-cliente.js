6f31a687c4217c78918a2a2c92686a0808f798f1
Samuel Arone <samu.arone@gmail.com>
2024-05-16 14:13:58 -0500

    test 1.0.1 fix politica

--- zona-no-cliente.js
+++ zona-no-cliente.js
@@ -19,7 +19,7 @@ window.localStorage.setItem("app-embedded", "on");
 //   : "/?token=aFdDaWNjL0pITER6dnBVdnhvRFUreVRKR1RUM3lSYWlXQWE1VktGRktEbjl0VlZvdXpqRVo2TVhnT0pheTZTRFJBSyt1Z09UaFAvOUU1ai9kdjZQNE53VFFrNjNnOWZ2V21wWCs4Z2VGTTF6aCtXd25ZZUgrdXJSSmJuV0VOb09UUXhtM0FIVUdob1ZVQ2JOSFlrak9BPT05MTg0MzBiYzI4NDkzNDVjMGZkNmQyODkyZTIzMTZjOGFiY2RjNjg3MjE2YWYzMWE0MGQzNDlkODBmNGRjMzhid";
 // const url = `${urlBase}${page}${token}`;
 
-const url = "http://localhost:3001/registro-app/";
+const url = "https://b58xfhbh-3001.brs.devtunnels.ms/registro-app/";
 
 const btn = document.getElementById("btnTest");
 btn.addEventListener("click", function () {
