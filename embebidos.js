e6cd6f4640d68275cd6cc01e96d56af7a1958f12
Samuel Arone <samu.arone@gmail.com>
2024-05-28 16:19:27 -0500

    test 1.0.3

--- embebidos.js
+++ embebidos.js
@@ -6,9 +6,10 @@ const txtInput = document.getElementById("txtInput");
 const getOptionsEmbedded = (value = "") => {
   const options = {
     1: "/bienestar-play",
-    2: "/bienestar-en-vivo",
-    3: "/rutas",
-    4: "/lista-rutas",
+    2: "/bienestar-en-vivo/?type=workshop",
+    3: "/bienestar-en-vivo/?type=live-program",
+    4: "/rutas",
+    5: "/lista-rutas",
   };
 
   return options[value] || "";
@@ -19,17 +20,19 @@ const getUrl = () => {
   let parametersToken = txtInput.value.trim();
   const pathBase = "https://web-test.estarbien.com.pe";
   const optionEmbedded = getOptionsEmbedded(option);
+  const enlace = optionEmbedded.includes("?") ? "&" : "/?";
 
   if (!pathBase || !optionEmbedded || !parametersToken) {
     return "";
   }
+
   parametersToken = parametersToken.replace("/?", "").replace("token=", "");
-  return `${pathBase}${optionEmbedded}/?token=${parametersToken}`;
+
+  return `${pathBase}${optionEmbedded}${enlace}token=${parametersToken}`;
 };
 
 const btn = document.getElementById("btnTest");
 btn.addEventListener("click", function () {
-  debugger;
   const url = getUrl();
   if (!url) {
     alert("Seleccione una opción válida");
