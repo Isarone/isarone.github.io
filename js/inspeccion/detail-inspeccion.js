f7d22d0195d59afdaaa5dba2eecb11bd94edb187
Samuel Arone <samu.arone@gmail.com>
2024-09-01 20:36:54 -0500

    app-camera : show camera

--- js/inspeccion/detail-inspeccion.js
+++ js/inspeccion/detail-inspeccion.js
@@ -1,18 +1,18 @@
-﻿
-document.querySelector("#btnNuevo").addEventListener("click", showModal);
-const photo = document.querySelector('#photo');
-const camera = document.querySelector('#camera');
-camera.addEventListener('change', function (e) {
-    photo.src = URL.createObjectURL(e.target.files[0]);
+﻿const photo = document.querySelector("#photo");
+const camera = document.querySelector("#camera");
+const btnNuevo = document.querySelector("#btnNuevo");
+btnNuevo.addEventListener("click", showModal);
+camera.addEventListener("change", function (e) {
+  photo.src = URL.createObjectURL(e.target.files[0]);
 });
 
-function showModal ()  {
-    console.log("entro")
-    const tabActive = document.querySelector(".nav .nav-link.active")?.dataset.text;
-    if (!tabActive) return;
+function showModal() {
+  camera.click();
+  const tabActive = document.querySelector(".nav .nav-link.active")?.dataset
+    .text;
+  if (!tabActive) return;
 
-    if (tabActive === "observacion") {
-        document.querySelector("#btnNuevaObservacion").click();
-    }
-
+  if (tabActive === "observacion") {
+    document.querySelector("#btnNuevaObservacion").click();
+  }
 }
